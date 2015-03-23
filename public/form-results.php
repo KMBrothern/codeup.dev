0f420829835fd9749b93d15b5a463a2283bfa9c5
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-23 17:12:49 -0500

    updated code to clean user inputs from forms

--- public/form-results.php
+++ public/form-results.php
@@ -5,13 +5,13 @@ $number = isset($_POST['number']) ? $_POST['number'] : '';
 <!DOCTYPE html>
 <html>
 <head>
-    <title>POST Example</title>
+    <title>POST Results</title>
 </head>
 <body>
     <h2>Name</h2>
-    <p><?php echo $name; ?></p>
+    <p><?php echo htmlspecialchars(strip_tags($name)); ?></p>
     <h2>Number</h2>
-    <p><?php echo $number; ?></p>
+    <p><?php echo htmlspecialchars(strip_tags($number)); ?></p>
     <a href="form-example.php">Back</a>
 </body>
 </html>

102cb2b047f30ec90c1e40b6022cdcc231879e33
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-23 17:13:50 -0500

    Working on login/authorization and redirects, and making generic functions for includes/requires

--- public/authorized.php
+++ public/authorized.php
@@ -1,3 +1,15 @@
+<?php
+// if (user is not logged in -same session) {
+//     header("Location: login.php");
+//     exit();
+// }else{
+//     $message = " You are authorized to view this page";
+// }
+session_start();
+var_dump($_SESSION);
+var_dump(session_id());
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,3 +19,4 @@
 <h1><strong>Authorized</strong></h1>
 </body>
 </html>
+<!-- Welcome,<?= $message ?> -->

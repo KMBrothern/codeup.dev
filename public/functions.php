af639b68d95fc628deacf866864166f32ebf14df
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-23 21:50:26 -0500

    Beginning to work on generic reusable code-functions exercise

---
+++ public/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function inputHas($key) {
+    if ($_) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+function inputGet($key) {
+    if (condition) {
+        return $key
+    } else {
+        return false;
+    }
+    
+}
+
+function escape($input){
+
+}
+
+?>

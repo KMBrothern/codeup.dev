c6419bddf765948ca9da47e616e4a9dfc20318c0
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-23 21:42:29 -0500

    redirects to authorized.php if login successful

--- public/login.php
+++ public/login.php
@@ -1,41 +1,30 @@
 <?php
 
 session_start();
-$sessionId = session_id();
+
+if (isset($_SESSION['LOGGED_IN_USER'])) {
+    header("Location:authorized.php");
+    exit();
+}
 
 $username = isset($_POST['username']) ? $_POST['username'] : '';
 $password = isset($_POST['password']) ? $_POST['password'] : '';
-
-// if (isset($_SESSION['LOGGED_IN_USER'])) {
-//     header("Location : authorized.php");
-//     exit();
-// }
-
 
 if ($username == '' && $password == '') {
     echo "Please enter your username and password";
 
 } else if($username == 'guest' && $password == 'password') {
     $_SESSION['LOGGED_IN_USER'] = $username;
-    header("Location: authorized.php");
+    header("Location:authorized.php");
     exit();
 }else {
     echo "Sorry, login failed.";
 }
 
-// if (isset($_SESSION['LOGGED_IN_USER'])) {
-//     header("Location : authorized.php");
-//     exit();
-// }
-
-
-
-
 
 var_dump($_POST);
 var_dump($_SESSION);
-// var_dump(session_id());
-var_dump($sessionId);
+var_dump(session_id());
 ?>
 
 <!DOCTYPE html>

bf419f44a82f73fa16fbeb7630c6964ae05bf427
Keyasha Brothern <kmbrothern85@gmail.com>
2015-03-26 21:45:27 -0500

    Working on calculator exercise-current setting event listeners for buttons

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,1 +1,19 @@
 // Now i need to build the functionality for my calculator to actually work
+console.log('calculator.js file linked correctly');
+// grab the elements that I need to set event listeners for
+// var operators = document.getElementById('operators');
+var equals = document.getElementById('equals');
+var numbers = document.getElementById('numbers');
+var clear = document.getElementById('clear');
+var clearAll = document.getElementById('clearall');
+document.getElementById('operators').addEventListener('click', pressButton, false);
+
+
+console.log(operators);// event listeners for the above elements
+// operators.addEventListener('click',pressButton , false);
+
+var pressButton = function(event)
+{
+    alert('You clicked an operator!');
+}
+

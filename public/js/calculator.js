79f59b599a9f3e99c29b15541f73510dba4f9f4c
Keyasha Brothern <kmbrothern85@gmail.com>
2015-04-02 14:25:04 -0500

    connecting to a db using pdo w/constants and requires

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,19 +1,43 @@
 // Now i need to build the functionality for my calculator to actually work
 console.log('calculator.js file linked correctly');
-// grab the elements that I need to set event listeners for
-// var operators = document.getElementById('operators');
-var equals = document.getElementById('equals');
-var numbers = document.getElementById('numbers');
-var clear = document.getElementById('clear');
-var clearAll = document.getElementById('clearall');
-document.getElementById('operators').addEventListener('click', pressButton, false);
+
+// div id = total is where the calculations will happen need to send input from clicks there
+
+$('a').click (function (event){
+    console.log('.data-key');
+});
+
+var pressButton = function(event)
+{
+    alert('you clicked a link tag');
+}
+
+document.getElementById('add').addEventListener('click', pressButton, false);
+document.getElementById('subtract').addEventListener('click', pressButton, false);
+document.getElementById('divide').addEventListener('click', pressButton, false);
+document.getElementById('multiply').addEventListener('click', pressButton, false);
+document.getElementById('equals').addEventListener('click', pressButton, false);
+document.getElementById('btn1').addEventListener('click', pressButton, false);
+document.getElementById('btn2').addEventListener('click', pressButton, false);
+document.getElementById('btn3').addEventListener('click', pressButton, false);
+document.getElementById('btn4').addEventListener('click', pressButton, false);
+document.getElementById('btn5').addEventListener('click', pressButton, false);
+document.getElementById('btn6').addEventListener('click', pressButton, false);
+document.getElementById('btn7').addEventListener('click', pressButton, false);
+document.getElementById('btn8').addEventListener('click', pressButton, false);
+document.getElementById('btn9').addEventListener('click', pressButton, false);
+document.getElementById('clear').addEventListener('click', pressButton, false);
+document.getElementById('btn10').addEventListener('click', pressButton, false);
+document.getElementById('clearall').addEventListener('click', pressButton, false);
+
+
+
 
 
 console.log(operators);// event listeners for the above elements
 // operators.addEventListener('click',pressButton , false);
 
-var pressButton = function(event)
-{
-    alert('You clicked an operator!');
-}
-
+// var pressButton = function(event)
+// {
+//     alert('you clicked a link tag');
+// }
